e341cfeb2614da4667dec3b0f9572a0dd16a7b68
Hosni10 <mohammedhosni25@gmail.com>
2025-11-23 15:34:07 +0200

    trying another port in send email logic

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -9,9 +9,7 @@ const sendEmail = async (to, subject, html) => {
     // Production transporter using environment variables
     transporter = nodemailer.createTransport({
       service: "gmail",
-      host: "smtp.gmail.com",
-      port: 587,
-      secure: false,
+      port: 465,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
